21e01136b0a99014c67c1c94b82c056093f66ab6
Neeraj Singh <869neerajsingh@gmail.com>
2024-05-03 18:54:01 +0530

    feat: create layout for categories bar using html and style it using css

--- src/components/categoriesBar/CategoriesBar.jsx
+++ src/components/categoriesBar/CategoriesBar.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
 import styles from "./CategoriesBar.module.css";
 
+const keywords = [
+  "All",
+  "React js",
+  "Angular js",
+  "React Native",
+  "use of API",
+  "Redux",
+  "Redux toolkit",
+  "Next.js",
+  "Firebase",
+  "SCSS",
+  "Bootstrap",
+  "Tailwind css",
+  "Podcast",
+  "GitHub",
+  "Coding",
+  "Cricket",
+  "English Songs",
+  "Anime",
+  "Hindi Songs",
+  "India",
+];
+
 const CategoriesBar = () => {
-  return <p>CategoriesBar</p>;
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  return (
+    <div className={styles.categoriesBar}>
+      {keywords.map((value, index) => (
+        <div
+          key={index}
+          onClick={() => setActiveCategory(value)}
+          className={activeCategory === value ? `${styles.active}` : ""}
+        >
+          {value}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
