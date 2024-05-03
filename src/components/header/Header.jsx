0d87d77e71a8ccba71145e76991aede6d3f07f43
Neeraj Singh <869neerajsingh@gmail.com>
2024-05-03 14:28:42 +0530

    feat: create header layout using html and style it using css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,40 @@
 import styles from "./Header.module.css";
 
+import { FaBars } from "react-icons/fa6";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdNotifications, MdApps } from "react-icons/md";
+
+import youtubeLogo from "../../assets/youtube.png";
+import profilePhoto from "../../assets/profile.png";
+
 const Header = () => {
-  return <div className="border border-dark">Header</div>
+  return (
+    <div className={`border border-dark ${styles.header}`}>
+      <div className={styles.headerStart}>
+        <FaBars size={28} className={styles.headerMenu} />
+        <div>
+          <img src={youtubeLogo} alt="YouTube Logo" className={styles.headerLogo} />
+          <span className={styles.youtubeName}>YouTube</span>
+        </div>
+      </div>
+      
+      <div className={styles.headerCenter}>
+        <form>
+          <input type="text" placeholder="Search" />
+          <div></div>
+          <button>
+            <AiOutlineSearch size={28} />
+          </button>
+        </form>
+      </div>
+
+      <div className={styles.headerEnd}>
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img src={profilePhoto} alt="Profile Photo" />
+      </div>
+    </div>
+  )
 };
 
 export default Header;
