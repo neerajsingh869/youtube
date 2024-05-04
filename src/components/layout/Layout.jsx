ffb20b8b2bcd856723fd4b7f86727347650e5e86
Neeraj Singh <869neerajsingh@gmail.com>
2024-05-04 17:05:39 +0530

    feat: shift router logic to main.jsx file and create seperate component for layout

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import styles from "./Layout.module.css";
+
+import Container from "react-bootstrap/esm/Container";
+import Header from "../header/Header";
+import Sidebar from "../sidebar/Sidebar";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Layout = ({children}) => {
+  const {accessToken, loading} = useSelector(state => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !accessToken) {
+      navigate('/login');
+    }
+  }, [loading, accessToken, navigate]);
+
+  const [sidebar, setSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
+  return (
+   <>
+    <Header toggleSidebar={toggleSidebar} />
+    <div className={styles.appContainer}>
+      <Sidebar sidebar={sidebar} toggleSidebar={toggleSidebar} />
+      <Container fluid className="app_main border border-warning">
+        {children}
+      </Container>
+    </div>
+   </>
+  )
+}
+
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 
-import App from './App.jsx'
 import './main.css';
 import store from "./redux/store.js";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Error from "./components/error/Error.jsx";
+import Layout from "./components/layout/Layout.jsx";
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+
+const routes = createRoutesFromElements(
+  <Route path="/" errorElement={<Error />}>
+    <Route index={true} element={
+      <Layout>
+        <Home />
+      </Layout>
+    } />
+    <Route path="login" element={<Login />} />
+  </Route>
+)
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
 )
